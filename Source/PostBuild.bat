cfcb226beef0b5de635a4db52e3bbf2d17ab8e2e
zzfng <zzfng103@zohomail.com>
2024-06-30 15:14:34 +0800

    忽略引用和输出的程序集，暂时移除多版本的维护/调试，可在本地跑起来先。

---
+++ Source/PostBuild.bat
@@ -0,0 +1,36 @@
+@echo off
+set project_dir=%1
+set project_name=%2
+set sepcial_version=%~3
+set configuration=%4
+
+call %project_dir%LOCALVARIABLES%sepcial_version%.bat
+set rimworld_dir=%LOCALVARIABLES_rimworld_dir%
+set harmony_dll=%LOCALVARIABLES_harmony_dll%
+if not exist %rimworld_dir% ( 
+echo %rimworld_dir% not exist	
+exit /b 0
+)
+set mod_name=%project_name%
+set mod_dir=%rimworld_dir%Mods\%mod_name%\
+
+set solution_dir=%project_dir:~0,-7%
+set source_dll=%project_dir%bin\%mod_name%.dll
+if not exist "%source_dll%" ( 
+echo %source_dll% not exist	
+exit /b 0
+)
+
+@echo off
+SETLOCAL ENABLEDELAYEDEXPANSION
+
+
+xcopy /y /d /e /f "%source_dll%" "%solution_dir%Assemblies\"
+xcopy /y /d /e /f "%source_dll%" "%mod_dir%Assemblies\"
+if "%special_version%"=="" (
+	xcopy /y /d /e /f "%source_dll%" "%solution_dir%%sepcial_version%\Assemblies\"
+	xcopy /y /d /e /f "%source_dll%" "%mod_dir%%sepcial_version%\Assemblies\"
+)
+for %%d in (About Defs Languages Patches Textures) do (
+	xcopy /i /y /e /d /f %solution_dir%%%d %mod_dir%%%d
+)
